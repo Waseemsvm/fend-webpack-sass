bfbd00d22424fa0ac5f8c03211ee815158a1dfa2
Waseemsvm <waseemsvm14@gmail.com>
2023-04-09 19:57:31 +0530

    api challenge

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,8 @@
+
+const API_KEY = 'fd8ef3061871c7bcb9043693b0d65d2a';
+let units = "imperial";
+
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -8,12 +13,24 @@ function handleSubmit(event) {
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
+        .then(res => {
+            return res.json()
+        })
+        .then(function (data) {
+            document.getElementById('results').innerHTML = data.message
+        })
+
+    let sZipCode = "90210";
+    const sUri = `https://api.openweathermap.org/data/2.5/weather?zip=${sZipCode},us&appid=${API_KEY}&units=${units}`
+
+    let rOutput = document.getElementById("output");
+    rOutput.textContent = "Loading Data";
+
+    fetch(sUri).then(async d => {
+        let result = await d.json();
+        rOutput.textContent = result.name;
     })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
+
 }
 
 export { handleSubmit }
